6f0e5f7cd31f5eb7ce250beb0b45d01e5eb94cd1
Hinyi <mrmck16@gmail.com>
2022-12-27 13:59:23 +0100

    School

--- Kata/Kata.cs
+++ Kata/Kata.cs
@@ -8,13 +8,6 @@ public static class Paper
 {      
     public static int Paperwork(int n, int m)
     {
-        if (n < 0 || m < 0)
-        {
-            return 0;
-        }
-        else
-        {
-            return n * m;
-        }
+        return (n < 0 || m < 0) ? 0 : m * n;
     }
 }

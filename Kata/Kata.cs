648e3e12c757862cc50d1bbea52232c323b305c4
Hinyi <mrmck16@gmail.com>
2022-12-28 12:10:20 +0100

    yes

--- Kata/Kata.cs
+++ Kata/Kata.cs
@@ -1,19 +1,33 @@
 ﻿using System.Data;
 using System;
 using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Kata;
 
-public class NthSeries {
-	
-    public static string seriesSum (int n)
+public class Kata
+{
+    public static string GoodVsEvil(string good, string evil)
     {
-        double result = 0;
-        for (int i = 0; i < n; i++)
+        var good_count = 0;
+        var evil_count = 0;
+        List<int> good_worth = new List<int>(){ 1, 2, 3, 3, 4, 10 };
+        List<int> evil_worth = new List<int>(){ 1, 2, 2, 2, 3, 5, 10 };
+        var good_army = good.Split(' ').ToArray();
+        var evil_army = evil.Split(' ').ToArray();
+
+        for (int i=0; i < evil_army.Length; i++)
         {
-            result = result + (1 / (1.0 + i * 3.0));
+            evil_count += Int32.Parse(evil_army[i]) * evil_worth[i];
+        }
+        for (int i=0; i< good_army.Length; i++)
+        {
+            good_count += Int32.Parse(good_army[i]) * good_worth[i];
         }
 
-        return result.ToString("F").Replace(",",".");
+        if (good_count > evil_count) return "Battle Result: Good triumphs over Evil";
+        else if (evil_count > good_count) return "Battle Result: Evil eradicates all trace of Good";
+        else return "Battle Result: No victor on this battle field";
     }
 }
--- Kata/UnitTest1.cs
+++ Kata/UnitTest1.cs
@@ -8,11 +8,20 @@ using System;
 public class Tests
 {
     [Test]
-    public void Test1() {
-        Assert.AreEqual("0.00", NthSeries.seriesSum(0));
+    public static void EvilShouldWin()
+    {
+        Assert.AreEqual("Battle Result: Evil eradicates all trace of Good", Kata.GoodVsEvil("1 1 1 1 1 1", "1 1 1 1 1 1 1"));
     }
+  
     [Test]
-    public void Test2() {
-        Assert.AreEqual("1.77", NthSeries.seriesSum(9));
+    public static void GoodShouldTriumph()
+    {
+        Assert.AreEqual("Battle Result: Good triumphs over Evil", Kata.GoodVsEvil("0 0 0 0 0 10", "0 1 1 1 1 0 0"));
+    }
+  
+    [Test]
+    public static void ShouldBeATie()
+    {
+        Assert.AreEqual("Battle Result: No victor on this battle field", Kata.GoodVsEvil("1 0 0 0 0 0", "1 0 0 0 0 0 0"));
     }
 }

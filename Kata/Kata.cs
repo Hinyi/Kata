ab5642dd4b5a35ecc435a48b6c2284023edcfe0f
Hinyi <mrmck16@gmail.com>
2022-12-28 09:55:26 +0100

    yes

--- Kata/Kata.cs
+++ Kata/Kata.cs
@@ -1,17 +1,27 @@
 ﻿using System.Data;
 using System;
-
+using System.Linq;
 
 namespace Kata;
 
 public static class Kata {
-    public static int Number(List<int[]> peopleListInOut)
+    public static string GetMiddle(string s)
     {
-        var result = 0;
-        foreach (var i in peopleListInOut)
+        string result = string.Empty;
+        var size = s.Length;
+        var a = s.ToArray();
+        if (size % 2 == 0)
         {
-            result += i[0] - i[1];
+            result = a[size/2-1].ToString() + a[size/2].ToString();
+        }
+        else
+        {
+            result = a[size/2].ToString();
         }
         return result;
+        
+        // return string.IsNullOrEmpty(s)
+        //     ? s
+        //     : s.Substring((s.Length - 1) / 2, 2 - s.Length % 2);
     }
 }

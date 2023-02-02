759c4dba290198bc48c8eac384857a3b28d0f67b
Hinyi <mrmck16@gmail.com>
2023-02-02 10:24:19 +0100

    4161

--- Kata/Kata.cs
+++ Kata/Kata.cs
@@ -9,74 +9,74 @@ namespace Kata;
 
 public class Kata
 {
-    private static List<long> numberToDigitList(long n)
-    {
-        List<long> digits = new List<long>();
-        var charArray = n.ToString().ToArray();
-        foreach (var item in charArray)
-        {
-            digits.Add(long.Parse(item.ToString()));
-        }
-
-        return digits;
-    }
-    public static long NextBiggerNumber(long n)
-    {
-        var size = n.ToString().ToArray().Length;
-        List<long> digArr = numberToDigitList(n);
-
-        if (size == 1)
-            return n;
-
-        int i = 0;
-        for (i = size-1; i > 0; i--)
-        {
-            if (digArr[i] > digArr[i - 1])
-                break;
-        }
-
-        if (i != 0)
-        {
-            for (int j = size-1; j >= i; j--)
+    // private static List<long> numberToDigitList(long n)
+    // {
+    //     List<long> digits = new List<long>();
+    //     var charArray = n.ToString().ToArray();
+    //     foreach (var item in charArray)
+    //     {
+    //         digits.Add(long.Parse(item.ToString()));
+    //     }
+    //
+    //     return digits;
+    // }
+    // public static long NextBiggerNumber(long n)
+    // {
+    //     var size = n.ToString().ToArray().Length;
+    //     List<long> digArr = numberToDigitList(n);
+    //
+    //     if (size == 1)
+    //         return n;
+    //
+    //     int i = 0;
+    //     for (i = size-1; i > 0; i--)
+    //     {
+    //         if (digArr[i] > digArr[i - 1])
+    //             break;
+    //     }
+    //
+    //     if (i != 0)
+    //     {
+    //         for (int j = size-1; j >= i; j--)
+    //         {
+    //             if (digArr[i - 1] < digArr[j])
+    //             {
+    //                 (digArr[j], digArr[i - 1]) = (digArr[i - 1], digArr[j]);
+    //                 break;
+    //             }
+    //         }
+    //     }
+    //     else
+    //     {
+    //         return -1;
+    //     }
+    //
+    //     long[] res = new long[size];
+    //     var ind = size - 1;
+    //     for (int j = 0; j < i; j++)
+    //         res[j] = digArr[j];
+    //     for (int j = i; j < res.Length; j++)
+    //         res[j] = digArr[ind--];
+    //
+    //     string str = string.Join("", res);
+    //     var result = Convert.ToInt64(str);
+    //     return result;
+        
+        public static long NextBiggerNumber(long n)
+        {        
+            String str = GetNumbers(n);
+            for (long i = n+1; i <= long.Parse(str); i++)
             {
-                if (digArr[i - 1] < digArr[j])
+                if(GetNumbers(n)==GetNumbers(i))
                 {
-                    (digArr[j], digArr[i - 1]) = (digArr[i - 1], digArr[j]);
-                    break;
+                    return i;
                 }
             }
+            return -1;        
         }
-        else
+        public static string GetNumbers(long number)
         {
-            return -1;
+            var result = string.Join("", number.ToString().ToCharArray().OrderByDescending(x => x));
+            return result;
         }
-
-        long[] res = new long[size];
-        var ind = size - 1;
-        for (int j = 0; j < i; j++)
-            res[j] = digArr[j];
-        for (int j = i; j < res.Length; j++)
-            res[j] = digArr[ind--];
-
-        string str = string.Join("", res);
-        var result = Convert.ToInt64(str);
-        return result;
-        
-        // public static long NextBiggerNumber(long n)
-        // {        
-        //     String str = GetNumbers(n);
-        //     for (long i = n+1; i <= long.Parse(str); i++)
-        //     {
-        //         if(GetNumbers(n)==GetNumbers(i))
-        //         {
-        //             return i;
-        //         }
-        //     }
-        //     return -1;        
-        // }
-        // public static string GetNumbers(long number)
-        // {
-        //     return string.Join("", number.ToString().ToCharArray().OrderByDescending(x => x));
-        // }
     }
-}

5734f3ad493dc205d0021e9dc86b8764ee612741
Hinyi <mrmck16@gmail.com>
2023-02-22 13:27:17 +0100

    add

--- Kata/Kata.cs
+++ Kata/Kata.cs
@@ -8,69 +8,22 @@ using static System.Math;
 
 namespace Kata;
 
-public class Kata
+public class Parentheses
 {
-    private static List<List<int>> arrToList(int[][] arr)
+    public static bool ValidParentheses(string input)
     {
-        int x = 1,y = 1;
-        List<List<int>> list = new List<List<int>>();
-        var tak = arr[x][x];
-        foreach (int[] item in arr)
+        var inputArr = input.ToCharArray();
+        var counter = 0;
+        foreach (var i in inputArr)
         {
-            foreach (int i in item)
-            {
-                list[x][y] = 1;
-                list[x][y] = i;
-                y++;
-            }
-            x++;
+            if (i == '(')
+                counter++;
+            if (i == ')')
+                counter--;
+            if(counter<0)
+                return false;
+                
         }
-
-        return list;
-    }
-    public static int[] Snail(int[][] array)
-    {
-        if (array[0].Length == 0)
-            return new int[] { };
-        int x=0, y = 0;
-        var size = array.Length;
-        int n = int.Parse(Pow(size,2).ToString());
-        int k = 0;
-        var arr = array;//arrToList(array);
-        List<int> result = new List<int>();
-        while (k < n)
-        {
-            for (int i = x; i < x+size; i++)
-            {
-                result.Add(arr[y][i]);
-                k++;
-            }
-            size--;
-            y++;
-            for (int i = y; i < y+size; i++)
-            {
-                result.Add(arr[i][x+size]);
-                // result[k] = arr[x][y];
-                k++;
-            }
-            for (int i = x + size -1; i >= x; i--)
-            {
-                result.Add(arr[y+size-1][i]);
-                k++;
-            }
-
-            size--;
-            
-            for (int i = y+size-1; i >= y; i--)
-            {
-                result.Add(arr[i][x]);
-                k++;
-            }
-
-            x++;
-        }
-
-        int[] output = result.ToArray();
-        return output;
+        return counter == 0;
     }
 }
--- Kata/UnitTest1.cs
+++ Kata/UnitTest1.cs
@@ -5,47 +5,18 @@ namespace Kata;
 using NUnit.Framework;
 using System;
 
-
-
-    [TestFixture]
-    public class KataTest
+[TestFixture]
+public class SolutionTest
+{
+    [Test]
+    public void SampleTest1()
     {
-        
-        [Test]
-        public void SnailTest1()
-        {
-            int[][] array = new int[3][]
-            {
-                new []{1, 2, 3},
-                new []{4, 5, 6},
-                new []{7, 8, 9}
-            };
-            var r = new[] { 1, 2, 3, 6, 9, 8, 7, 4, 5 };
-            Test(array, r);
-        }        
-        [Test]
-        public void SnailTest2()
-        {
-            int[][] array = new int[][]
-            {
-                new int[]{},
-                new int[]{},
-                new int[]{}
-            };
-            var r = new int[] {};
-            Test(array, r);
-        }
-
-        public string Int2dToString(int[][] a)
-        {
-            return $"[{string.Join("\n", a.Select(row => $"[{string.Join(",", row)}]"))}]";
-        }
-
-        public void Test(int[][] array, int[] result)
-        {
-            var text = $"{Int2dToString(array)}\nshould be sorted to\n[{string.Join(",", result)}]\n";
-            Console.WriteLine(text);
-            Assert.AreEqual(result, Kata.Snail(array));
-        }
+        Assert.AreEqual(true, Parentheses.ValidParentheses( "()" ));
     }
-
+    
+    [Test]
+    public void SampleTest2()
+    {
+        Assert.AreEqual(false, Parentheses.ValidParentheses( ")(((()))" ));
+    }
+}
